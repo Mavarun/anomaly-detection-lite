8b062290a6d42d2233826559586c1c9843837500
M.Varun <116015799+Mavarun@users.noreply.github.com>
2026-09-06 00:56:34 +0530

    detectors: IsolationForest and OneClassSVM wrappers

--- src/anomaly_lite/__init__.py
+++ src/anomaly_lite/__init__.py
@@ -1,3 +1,10 @@
 """anomaly-lite: IsolationForest / OneClassSVM slice with precision@k."""
 
+from anomaly_lite.detectors import IsolationForestDetector, OneClassSVMDetector
+
+__all__ = [
+    "IsolationForestDetector",
+    "OneClassSVMDetector",
+]
+
 __version__ = "0.1.0"
